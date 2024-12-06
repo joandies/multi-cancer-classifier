9641e136cff79933c9b8ce0291d35de4a86d184a
jdies99 <jdies@visual-engin.com>
2024-12-06 20:22:05 +0100

    started with resnet50 model, needs transforms

---
+++ src/models/cervical_cancer_model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torchvision.models import ResNet50_Weights
+
+class CervicalModel(nn.Module):
+    def __init__(self, num_classes):
+        super(CervicalModel, self).__init__()
+        
+        # Load a pre-trained model (e.g., ResNet50)
+        self.base_model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
+        
+        # Freeze the base model's layers (so they don't get updated during training)
+        for param in self.base_model.parameters():
+            param.requires_grad = False
+        
+        # Replace the final layer to match the number of classes in our dataset
+        self.base_model.fc = nn.Linear(self.base_model.fc.in_features, num_classes)
+
+    def forward(self, x):
+        return self.base_model(x)
+
+# Number of classes (subclasses of cervical cancer)
+num_classes = 5
+model = CervicalModel(num_classes)
+
+# Print the model architecture
+print(model)
